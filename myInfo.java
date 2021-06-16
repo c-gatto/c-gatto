c11922c8f4ad06cf8a30ef6cb632fab1c44c91a6
Casey <85979603+c-gatto@users.noreply.github.com>
2021-06-16 03:10:37 -0500

    about me
    a small about me page

---
+++ myInfo.java
@@ -0,0 +1,25 @@
+package gitHubInfo;
+
+public class myInfo {
+
+	public static void main(String[] args) {
+		String name = "Casey Gatto";
+		String hometown = "Chicago, IL";
+		String book = "Conversations with Friends by Sally Rooney.";
+
+			System.out.println("ABOUT ME");
+			System.out.println("My name is "+name+" and I'm originally from "+hometown+", but I moved to Fayetteville, AR\nfor school in August 2020. Moving to Arkansas has been a unique and exciting experience,\nand I can't wait to progress further in school. The thing that makes me most excited about living here is \nthe vast expanse of mountain biking and hiking trails.");
+			System.out.println();
+			System.out.println("WHAT MADE YOU INTERESTED IN LEARNING HOW TO PROGRAM?");
+			System.out.println("My initial interest in programming started at a young age, inspired by the rise\nin video game complexity and popularity, as well as the growing of the\ninternet and website usage.");
+			System.out.println();
+			System.out.println("IF YOU COULD ONLY READ ONE MORE BOOK FOR THE REST OF YOUR LIFE, \nWHICH BOOK WOULD IT BE AND WHY?");
+			System.out.println("If I could only read one more book for the rest of my life, it would\nbe "+book+" I would choose this book\nbecause Rooney has a particular writing style that I am quite fond of,\nand I appreciate the intricacy she provides to her characters and storyline.");
+			
+			System.out.println();
+			
+			System.out.println("tldr; Casey Gatto, Computer Science Major @ UARK, Likes to mountain bike and read.");
+			
+	}
+
+}
